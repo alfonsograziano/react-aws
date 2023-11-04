42b383c73bd75fe46f74ac1b507b6b960646558a
Alfonso Graziano <alfonso.graziano@nearform.com>
2023-11-04 09:40:22 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Let's deploy this on AWS 🚀
+          Let's deploy this on AWS 🚀 (staging env)
         </p>
         <a
           className="App-link"
